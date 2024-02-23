d93d6fc370623a60d792543094f496409d3d81de
Boris LECLERE <Borisleclere@gmail.com>
2024-02-23 16:37:40 +0100

    Move basic conversion functions to converters.py

--- src/ffbb_api_client/api_types.py
+++ src/ffbb_api_client/api_types.py
@@ -1,82 +1,22 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
-from typing import Any, Callable, List, Optional, Type, TypeVar, Union, cast
+from typing import Any, List, Optional, Union
 
-import dateutil.parser
-
-T = TypeVar("T")
-
-
-class CompetitionType(Enum):
-    CHAMPIONSHIP = "championship"
-    CUP = "cup"
-
-
-def from_none(x: Any) -> Any:
-    assert x is None
-    return x
-
-
-def from_str(x: Any) -> str:
-    assert isinstance(x, str)
-    return x
-
-
-def from_union(fs, x):
-    for f in fs:
-        try:
-            return f(x)
-        except Exception:
-            pass
-    assert False
-
-
-def from_float(x: Any) -> float:
-    assert isinstance(x, (float, int)) and not isinstance(x, bool)
-    return float(x)
-
-
-def is_type(t: Type[T], x: Any) -> T:
-    assert isinstance(x, t)
-    return x
-
-
-def to_float(x: Any) -> float:
-    assert isinstance(x, float)
-    return x
-
-
-def from_int(x: Any) -> int:
-    assert isinstance(x, int) and not isinstance(x, bool)
-    return x
-
-
-def from_stringified_bool(x: str) -> bool:
-    if x == "true":
-        return True
-    if x == "false":
-        return False
-    assert False
-
-
-def from_datetime(x: Any) -> datetime:
-    return dateutil.parser.parse(x)
-
-
-def to_class(c: Type[T], x: Any) -> dict:
-    assert isinstance(x, c)
-    return cast(Any, x).to_dict()
-
-
-def from_bool(x: Any) -> bool:
-    assert isinstance(x, bool)
-    return x
-
-
-def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
-    return [f(y) for y in x]
+from .converters import (
+    from_bool,
+    from_datetime,
+    from_float,
+    from_int,
+    from_list,
+    from_none,
+    from_str,
+    from_stringified_bool,
+    from_union,
+    is_type,
+    to_class,
+    to_float,
+)
 
 
 @dataclass
@@ -785,6 +725,11 @@ def area_from_dict(s: Any) -> List[Area]:
 
 def area_to_dict(x: List[Area]) -> Any:
     return from_list(lambda x: to_class(Area, x), x)
+
+
+class CompetitionType(Enum):
+    CHAMPIONSHIP = "championship"
+    CUP = "cup"
 
 
 @dataclass
---
+++ src/ffbb_api_client/converters.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from typing import Any, Callable, List, Type, TypeVar, cast
+
+import dateutil.parser
+
+T = TypeVar("T")
+
+
+def from_none(x: Any) -> Any:
+    assert x is None
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except Exception:
+            pass
+    assert False
+
+
+def from_float(x: Any) -> float:
+    assert isinstance(x, (float, int)) and not isinstance(x, bool)
+    return float(x)
+
+
+def is_type(t: Type[T], x: Any) -> T:
+    assert isinstance(x, t)
+    return x
+
+
+def to_float(x: Any) -> float:
+    assert isinstance(x, float)
+    return x
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_stringified_bool(x: str) -> bool:
+    if x == "true":
+        return True
+    if x == "false":
+        return False
+    assert False
+
+
+def from_datetime(x: Any) -> datetime:
+    return dateutil.parser.parse(x)
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
+def from_bool(x: Any) -> bool:
+    assert isinstance(x, bool)
+    return x
+
+
+def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
+    assert isinstance(x, list)
+    return [f(y) for y in x]
